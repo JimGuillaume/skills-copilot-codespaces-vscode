f29b72ac78273ea3022f923675f301eb65794264
Jim <148584426+JimGuillaume@users.noreply.github.com>
2024-11-20 18:48:30 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create web server
+const express = require('express');
+const app = express();
+// Create a server
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
